bcd5541e3113807bd8f2a48625c71c494f5f2c02
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-03-26 12:38:00 +0100

    create yes or no question domain model.

---
+++ Project/ConsoleProject/Models/Questions/YesOrNoQuestion.cs
@@ -0,0 +1,9 @@
+namespace ConsoleProject.Models;
+
+public class YesOrNoQuestion
+{
+    public bool Choice {get; set;}
+    public bool DisqualifyForNoChoice {get; set;}
+    // public Guid QuestionId {get; set;}
+    // public Question Question {get; set;}
+}
