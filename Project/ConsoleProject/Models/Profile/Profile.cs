377b9e9f80173c53bafa9335053bdc34009f0cd7
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-03-26 12:00:34 +0100

    create profile domain model.

---
+++ Project/ConsoleProject/Models/Profile/Profile.cs
@@ -0,0 +1,8 @@
+namespace ConsoleProject.Models;
+
+public class Profile 
+{
+    public EducationProfile EducationProfile {get; set;}
+    public ProfessionalProfile ProfessionalProfile {get; set;}
+    public Resume Resume {get; set;}
+}
