f055ba63e06635f32c126a9536c4e54e19d2b0a1
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-03-26 12:35:38 +0100

    create vedio based question domain model.

---
+++ Project/ConsoleProject/Models/Questions/VideoBasedQuestion.cs
@@ -0,0 +1,7 @@
+namespace ConsoleProject.Models;
+
+public class VideoBasedQuestion
+{
+    public string AdditionalSubmissionInformation{get; set;}
+    public string MaxDurationOfVideo {get; set;}
+}
