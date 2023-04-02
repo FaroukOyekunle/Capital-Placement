96fbf6f604b14175826afe13d3541a0f3eb070ea
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-04-02 20:16:03 +0100

    implement question category type enum class.

---
+++ Project/ConsoleProject/Enums/QuestionType.cs
@@ -0,0 +1,14 @@
+namespace ConsoleProject.Enums;
+
+public enum QuestionType
+{
+    Paragraph = 1,
+    ShortAnswer,
+    YesOrNo,
+    Dropdown,
+    MultipleChoice,
+    Date,
+    Number,
+    VideoQuestion,
+    FileUpload
+}
