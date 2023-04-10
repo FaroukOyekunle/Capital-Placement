94f43c8694ae9a57f33ed238d772c6f0f5836c39
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-04-10 22:06:38 +0100

    create base response class model.

---
+++ Project/ConsoleProject/DTOs/RetrievalModels/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject.DTOs.RetrievalModels
+{
+    public class BaseResponse<T>
+    {
+       public T Data {get; set;}
+       public bool Status {get; set;}
+       public string Message {get; set;}
+    }
+}
