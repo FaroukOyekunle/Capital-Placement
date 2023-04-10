4d28b633c0bf06df63c84e2bf9b35ab969da7901
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-04-10 22:07:36 +0100

    create project preview service model.

---
+++ Project/ConsoleProject/DTOs/RetrievalModels/PreviewServiceModel.cs
@@ -0,0 +1,8 @@
+namespace ConsoleProject.DTOs.RetrievalModels
+{
+    public class PreviewServiceModel
+    {
+        public string ApplicationCoverImage {get; set;}
+        public ProgramModel ProgramModel {get; set;}
+    }
+}
