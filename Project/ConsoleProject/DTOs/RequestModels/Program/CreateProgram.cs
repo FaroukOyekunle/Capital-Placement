d91ebb097439f2244f10176d36e67bbccf920b00
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-04-11 01:12:07 +0100

    create DTOs request model create program class

---
+++ Project/ConsoleProject/DTOs/RequestModels/Program/CreateProgram.cs
@@ -0,0 +1,21 @@
+using ConsoleProject.Enums;
+using ConsoleProject.Models;
+
+namespace ConsoleProject.DTOs.RequestModels;
+
+public class CreateProgram
+{
+    public string ProgramTitle { get; set; }
+    public string Description { get; set; }
+    public List<Skills> ApplicantSkills { get; set; } = new List<Skills>();
+    public List<string> Benefits { get; set; } = new List<string>();
+    public List<string> ApplicationCriteria { get; set; } = new List<string>();
+    public ProgramType ProgramType { get; set; }
+    public DateTime ProgramStart { get; set; }
+    public DateTime ApplicationStart { get; set; }
+    public DateTime ApplicationEnds { get; set; }
+    public MinQualification ApplicantQualification { get; set; }
+    public List<ProgramLocation> ProgramLocations { get; set; } = new List<ProgramLocation>();
+    public int DurationInMonths { get; set; }
+    public int MaxApplications { get; set; }
+}
