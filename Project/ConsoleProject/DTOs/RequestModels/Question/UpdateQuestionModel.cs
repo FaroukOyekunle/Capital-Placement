3a391263982fabac97e63753b2a59a1697710ad5
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-04-14 20:21:55 +0100

    questions_model

---
+++ Project/ConsoleProject/DTOs/RequestModels/Question/UpdateQuestionModel.cs
@@ -0,0 +1,7 @@
+namespace ConsoleProject.DTOs.RequestModels;
+
+public class UpdateQuestionModel
+{
+    public string Response {get; set;}
+    public string QuestionContent {get; set;}
+}
---
+++ Project/ConsoleProject/DTOs/RequestModels/Question/VideoQuestionModel.cs
@@ -0,0 +1,7 @@
+namespace ConsoleProject.DTOs.RequestModels;
+
+public class VideoQuestionModel 
+{
+   public string AdditionalSubmissionInformation  {get; set;}
+    public string MaxDurationOfVideo {get; set;}
+}
