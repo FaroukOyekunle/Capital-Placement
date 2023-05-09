3c2b82ee8c70add46d051869d3dcd0ae23b1a42f
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-05-09 11:09:46 +0100

    implement Question Repository with code fix.

---
+++ Project/WebAPIProject/Implementations/Repositories/QuestionRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskConsole.Models;
+using TaskProjectWebAPI.Context;
+using TaskProjectWebAPI.Interfaces.Repositories;
+
+namespace TaskProjectWebAPI.Implementations.Repositories
+{
+    public class QuestionRepository : GenericRepository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
