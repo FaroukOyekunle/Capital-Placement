d65581e46183e1db69a6b33d2235eda86754de1f
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-05-09 11:11:59 +0100

    implement stage repository with code fix.

---
+++ Project/WebAPIProject/Implementations/Repositories/StageRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskConsole.Models;
+using TaskProjectWebAPI.Context;
+using TaskProjectWebAPI.Interfaces.Repositories;
+
+namespace TaskProjectWebAPI.Implementations.Repositories
+{
+    public class StageRepository : GenericRepository<Stage>, IStageRepository
+    {
+        public StageRepository(ApplicationContext context) : base(context)
+        {
+            
+        }
+    }
+}
