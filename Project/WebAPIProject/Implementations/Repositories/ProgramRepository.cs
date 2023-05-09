7004f8303301228423f7895a216719c0a103417c
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-05-09 11:08:44 +0100

    implement Program Repository with code fix.

---
+++ Project/WebAPIProject/Implementations/Repositories/ProgramRepository.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleProject.Models;
+using WebAPIProject.Context;
+using WebAPIProject.Interfaces.Repositories;
+
+namespace WebAPIProject.Implementations.Repositories
+{
+    public class ProgramRepository : GenericRepository<ConsoleProject.Models.Program>, IProgramRepository
+    {
+        public ProgramRepository(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
