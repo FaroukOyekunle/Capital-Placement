b939aab80109481b7415ba0f3cdb1560d13ad6a7
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-05-11 11:01:24 +0100

    created IProgram interface repository with dependency update.

---
+++ Project/WebAPIProject/Interfaces/Repositories/IProgramRepository.cs
@@ -0,0 +1,8 @@
+﻿using TaskConsole.Models;
+
+namespace TaskProjectWebAPI.Interfaces.Repositories
+{
+    public interface IProgramRepository : IGenericRepository<TaskConsole.Models.Program>
+    {
+    }
+}
