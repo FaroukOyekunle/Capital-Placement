a1f75aa7228c1c3df9dc7bfd7c3728fa0a07b309
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-05-12 23:56:44 +0100

    create IProgram Details Service with with dependency update and bug fix.

---
+++ Project/WebAPIProject/Interfaces/Services/IProgramDetailsService.cs
@@ -0,0 +1,17 @@
+﻿using TaskConsole.DTOs.RetrievalModels;
+using TaskConsole.DTOs.RequestModels;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TaskProjectWebAPI.Interfaces.Services
+{
+    public interface IProgramDetailsService
+    {
+        Task<BaseResponse<bool>> AddProgramAsync(CreateProgram programModel);
+        Task<BaseResponse<bool>> EditProgramAsync(UpdateProgram programModel, string programTitle);
+        Task<BaseResponse<IEnumerable<ProgramModel>>> GetAllProgramsAsync();
+        Task<BaseResponse<ProgramModel>> GetProgramAsync(string Id);
+        Task<BaseResponse<ProgramModel>> GetProgramByTitleAsync(string programTitle);
+        Task<BaseResponse<bool>> DeleteProgramAsync(string Id);
+    }
+}
