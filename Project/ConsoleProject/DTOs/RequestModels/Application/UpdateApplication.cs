0c166b458fba3dfcf293a00536d5e59c4e0c22ee
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-22 17:37:51 +0100

    Add UpdateApplication DTO with data annotations
    UpdateApplication class, which serves as a Data Transfer Object (DTO) for updating an existing application. Each property in the DTO is annotated with data annotations to enforce required fields and validation rules. The DTO includes information such as the applicant's updated first name, last name, phone number, nationality, and current residence.
    
    By using data annotations, we ensure that essential information is provided when updating an application, and errors are appropriately handled with descriptive error messages. This enhances the reliability and usability of the ConsoleProject application when processing updates to existing applications.

--- Project/ConsoleProject/DTOs/RequestModels/Application/UpdateApplication.cs
+++ Project/ConsoleProject/DTOs/RequestModels/Application/UpdateApplication.cs
@@ -1,14 +1,25 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations; 
 
-namespace ConsoleProject.DTOs.RequestModels;
-
-public class UpdateApplication
+namespace ConsoleProject.DTOs.RequestModels
 {
-    [Required(ErrorMessage ="Fill in The First Name")]
-    public string FirstName{get; set;}
-    [Required(ErrorMessage ="Fill in The Last Name")]
-    public string LastName {get; set;}
-    public string PhoneNumber {get; set;}
-    public string Nationality {get; set;}
-    public string CurrentResidence {get; set;}
+    // UpdateApplication class represents the data transfer object (DTO) used for updating an application
+    public class UpdateApplication
+    {
+        // [Required] attribute ensures that FirstName property must be provided and cannot be null or empty
+        [Required(ErrorMessage = "Fill in The First Name")]
+        public string FirstName { get; set; }
+
+        // [Required] attribute ensures that LastName property must be provided and cannot be null or empty
+        [Required(ErrorMessage = "Fill in The Last Name")]
+        public string LastName { get; set; }
+
+        // PhoneNumber property represents the applicant's updated phone number
+        public string PhoneNumber { get; set; }
+
+        // Nationality property represents the applicant's updated nationality
+        public string Nationality { get; set; }
+
+        // CurrentResidence property represents the applicant's updated current residence
+        public string CurrentResidence { get; set; }
+    }
 }
