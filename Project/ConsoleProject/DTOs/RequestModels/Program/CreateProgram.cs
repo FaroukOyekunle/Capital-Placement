27b080d8a304829d8a605bc0d95293243f8a767f
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-23 17:38:48 +0100

    Add CreateProgram DTO for creating new programs
    CreateProgram class, which serves as a Data Transfer Object (DTO) for creating new programs in the ConsoleProject application. The DTO includes properties to capture various program details, such as the program title, description, required applicant skills, program benefits, application criteria, program type, start and end dates, applicant qualification, program locations, duration in months, and maximum allowed applications.
    
    By utilizing this DTO, the application can efficiently gather all the necessary information when creating new programs, enabling a streamlined and organized program creation process. The default initialization of certain properties ensures that empty lists are used when no values are provided, enhancing the flexibility and robustness of the CreateProgram DTO.

--- Project/ConsoleProject/DTOs/RequestModels/Program/CreateProgram.cs
+++ Project/ConsoleProject/DTOs/RequestModels/Program/CreateProgram.cs
@@ -1,21 +1,53 @@
 using ConsoleProject.Enums;
-using ConsoleProject.Models;
+using ConsoleProject.Models; 
 
-namespace ConsoleProject.DTOs.RequestModels;
-
-public class CreateProgram
+namespace ConsoleProject.DTOs.RequestModels
 {
-    public string ProgramTitle { get; set; }
-    public string Description { get; set; }
-    public List<Skills> ApplicantSkills { get; set; } = new List<Skills>();
-    public List<string> Benefits { get; set; } = new List<string>();
-    public List<string> ApplicationCriteria { get; set; } = new List<string>();
-    public ProgramType ProgramType { get; set; }
-    public DateTime ProgramStart { get; set; }
-    public DateTime ApplicationStart { get; set; }
-    public DateTime ApplicationEnds { get; set; }
-    public MinQualification ApplicantQualification { get; set; }
-    public List<ProgramLocation> ProgramLocations { get; set; } = new List<ProgramLocation>();
-    public int DurationInMonths { get; set; }
-    public int MaxApplications { get; set; }
+    // CreateProgram class represents the data transfer object (DTO) used for creating a new program
+    public class CreateProgram
+    {
+        // ProgramTitle property represents the title of the new program
+        public string ProgramTitle { get; set; }
+
+        // Description property represents the description of the new program
+        public string Description { get; set; }
+
+        // ApplicantSkills property represents a list of skills required for the program,
+        // initialized as an empty list by default
+        public List<Skills> ApplicantSkills { get; set; } = new List<Skills>();
+
+        // Benefits property represents a list of benefits offered by the program,
+        // initialized as an empty list by default
+        public List<string> Benefits { get; set; } = new List<string>();
+
+        // ApplicationCriteria property represents a list of criteria for application,
+        // initialized as an empty list by default
+        public List<string> ApplicationCriteria { get; set; } = new List<string>();
+
+        // ProgramType property represents the type of the program, using the custom ProgramType enumeration
+        public ProgramType ProgramType { get; set; }
+
+        // ProgramStart property represents the start date of the program
+        public DateTime ProgramStart { get; set; }
+
+        // ApplicationStart property represents the start date of program applications
+        public DateTime ApplicationStart { get; set; }
+
+        // ApplicationEnds property represents the end date of program applications
+        public DateTime ApplicationEnds { get; set; }
+
+        // ApplicantQualification property represents the minimum qualification required for applicants,
+        // using the custom MinQualification enumeration
+        public MinQualification ApplicantQualification { get; set; }
+
+        // ProgramLocations property represents a list of locations where the program will take place,
+        // initialized as an empty list by default
+        public List<ProgramLocation> ProgramLocations { get; set; } = new List<ProgramLocation>();
+
+        // DurationInMonths property represents the duration of the program in months
+        public int DurationInMonths { get; set; }
+
+        // MaxApplications property represents the maximum number of applications allowed for the program
+        public int MaxApplications { get; set; }
+    }
 }
