c33e217a31c13e1ca9f63faff9e852fcfdf8b614
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-24 11:03:47 +0100

    Add NumberQuestionModel DTO
    NumberQuestionModel, which is a Data Transfer Object (DTO) used for handling Number type questions within the application.
    
    The NumberQuestionModel includes the following property:
    
    NumberQuestion: Represents the numeric value of the question. This property will store the numeric input provided by the user in response to the number-based question.

--- Project/ConsoleProject/DTOs/RequestModels/Question/NumberQuestionModel.cs
+++ Project/ConsoleProject/DTOs/RequestModels/Question/NumberQuestionModel.cs
@@ -1,7 +1,9 @@
 namespace ConsoleProject.DTOs.RequestModels
 {
+    // NumberQuestionModel class represents the data transfer object (DTO) for Number type questions
     public class NumberQuestionModel
     {
-        public int NumberQuestion{get; set;}
+        // NumberQuestion property represents the numeric value of the question
+        public int NumberQuestion { get; set; }
     }
 }
