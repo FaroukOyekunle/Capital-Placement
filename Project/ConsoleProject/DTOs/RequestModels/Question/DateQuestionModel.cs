a16a8baac25f0fbfa4b54c35476fbfe31adbd043
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-24 10:50:19 +0100

    Add DateQuestionModel DTO
    DateQuestionModel, which is a Data Transfer Object (DTO) used for handling Date type questions within the application.
    
    The DateQuestionModel includes the following property:
    
    DateQuestion: Represents the DateTime value for the Date type question. This property will be used to capture the date input from the user for Date type questions.
    
    By utilizing the DateQuestionModel, the application can effectively handle Date type questions and ensure accurate data storage and processing for this specific question type.

--- Project/ConsoleProject/DTOs/RequestModels/Question/DateQuestionModel.cs
+++ Project/ConsoleProject/DTOs/RequestModels/Question/DateQuestionModel.cs
@@ -1,7 +1,9 @@
 namespace ConsoleProject.DTOs.RequestModels
 {
+    // DateQuestionModel class represents the data transfer object (DTO) for Date type questions
     public class DateQuestionModel
     {
-        public DateTime DateQuestion {get; set;}
+        // DateQuestion property represents the DateTime value for the Date type question
+        public DateTime DateQuestion { get; set; }
     }
 }
