af562449c627276c9fcb08a872110e9c81dbc7a1
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 10:56:44 +0100

    Add Gender enumeration
     adds the Gender enumeration to the application. The Gender enum represents different gender options, including Male, Female, RatherNotSay (when a user prefers not to disclose their gender), and Other (for other gender options not explicitly listed). Each option has an associated integer value for identification and handling in the application.

--- Project/ConsoleProject/Enums/Gender.cs
+++ Project/ConsoleProject/Enums/Gender.cs
@@ -1,9 +1,18 @@
-namespace ConsoleProject.Enums;
-
-public enum Gender
+namespace ConsoleProject.Enums
 {
-    Male=1,
-    Female,
-    RatherNotSay,
-    Other,
+    // Gender enumeration represents different gender options.
+    public enum Gender
+    {
+        // Male represents the male gender option and has a value of 1.
+        Male = 1,
+
+        // Female represents the female gender option and has a value of 2.
+        Female,
+
+        // RatherNotSay represents the option when a user prefers not to disclose their gender and has a value of 3.
+        RatherNotSay,
+
+        // Other represents other gender options that are not explicitly listed and has a value of 4.
+        Other,
+    }
 }
