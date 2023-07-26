c76410010150498574fa5caad68ae1cb2f41faa6
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 10:49:31 +0100

    Add StageModel DTO
    dds the StageModel data transfer object (DTO) to the application. The StageModel class represents a retrieval model for a stage and includes properties such as StageName, StageType, and VideoInterviewQuestion. The StageType property uses the StageType enumeration, and the VideoInterviewQuestion property represents the video interview question associated with the stage.

--- Project/ConsoleProject/DTOs/RetrievalModels/Stages/StageModel.cs
+++ Project/ConsoleProject/DTOs/RetrievalModels/Stages/StageModel.cs
@@ -1,11 +1,18 @@
 using ConsoleProject.Enums;
-using ConsoleProject.Models;
+using ConsoleProject.Models; 
 
-namespace ConsoleProject.DTOs.RetrievalModels;
-
-public class StageModel
+namespace ConsoleProject.DTOs.RetrievalModels
 {
-    public string StageName { get; set; }
-    public StageType StageType { get; set; }
-    public VideoInterviewQuestion VideoInterviewQuestion { get; set; }
+    // StageModel represents the data transfer object (DTO) for a stage retrieval model.
+    public class StageModel
+    {
+        // StageName property holds the name of the stage.
+        public string StageName { get; set; }
+
+        // StageType property holds the type of the stage. It uses the StageType enumeration.
+        public StageType StageType { get; set; }
+
+        // VideoInterviewQuestion property holds the video interview question associated with the stage.
+        public VideoInterviewQuestion VideoInterviewQuestion { get; set; }
+    }
 }
