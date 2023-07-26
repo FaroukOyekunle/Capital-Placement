7384e7308364e4a91786b4980da9bdc8d9da1f50
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 11:31:04 +0100

    Add QuestionType enumeration
    adds the QuestionType enumeration to the application. The enum represents different types of questions that can be used in the application's survey or questionnaire functionality. The enum contains various question types, such as Paragraph, ShortAnswer, YesOrNo, Dropdown, MultipleChoice, Date, Number, VideoQuestion, and FileUpload. These types will be used to categorize and differentiate the various types of questions that can be presented to users during the survey or questionnaire process.

--- Project/ConsoleProject/Enums/QuestionType.cs
+++ Project/ConsoleProject/Enums/QuestionType.cs
@@ -1,14 +1,33 @@
-namespace ConsoleProject.Enums;
-
-public enum QuestionType
+namespace ConsoleProject.Enums
 {
-    Paragraph = 1,
-    ShortAnswer,
-    YesOrNo,
-    Dropdown,
-    MultipleChoice,
-    Date,
-    Number,
-    VideoQuestion,
-    FileUpload
+    // QuestionType enumeration represents different types of questions.
+    public enum QuestionType
+    {
+        // Paragraph type represents a question that requires a paragraph-length response.
+        Paragraph = 1,
+
+        // ShortAnswer type represents a question that requires a short textual response.
+        ShortAnswer,
+
+        // YesOrNo type represents a question that requires a Yes or No response.
+        YesOrNo,
+
+        // Dropdown type represents a question with a predefined list of options to choose from.
+        Dropdown,
+
+        // MultipleChoice type represents a question with multiple options, allowing users to choose more than one.
+        MultipleChoice,
+
+        // Date type represents a question that requires a date as a response.
+        Date,
+
+        // Number type represents a question that requires a numerical response.
+        Number,
+
+        // VideoQuestion type represents a question that requires a video-based response.
+        VideoQuestion,
+
+        // FileUpload type represents a question that allows users to upload files as a response.
+        FileUpload
+    }
 }
