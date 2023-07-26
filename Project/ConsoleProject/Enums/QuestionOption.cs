246ce2ebf2f4379952a300f8af5d3343bda24e3e
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 11:18:26 +0100

    Add QuestionOption enumeration

--- Project/ConsoleProject/Enums/QuestionOption.cs
+++ Project/ConsoleProject/Enums/QuestionOption.cs
@@ -1,5 +1,13 @@
-namespace ConsoleProject.Enums;
-
-public enum QuestionOption
+namespace ConsoleProject.Enums
 {
+    // QuestionOption enumeration represents options for a question.
+    // Note: This enum currently does not contain any options and needs to be populated.
+    public enum QuestionOption
+    {
+        // TODO: Add question options
+        //  Option1,
+        //  Option2,
+        //  Option3,
+        //   ...
+    }
 }
