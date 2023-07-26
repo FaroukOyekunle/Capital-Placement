022da90aac67a3a7360805605f23c558f2cf1b7f
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 11:37:33 +0100

    Add Skills enumeration
    adds the Skills enumeration to the application. The enum represents different skills that can be associated with an applicant or program. It includes skills such as SocialMedia, UIUx, SEO, ContentWriting, and GraphicsDesign. These skills can be utilized in the application's applicant profiles or program details to highlight the expertise required or possessed by applicants or programs.

--- Project/ConsoleProject/Enums/Skills.cs
+++ Project/ConsoleProject/Enums/Skills.cs
@@ -1,9 +1,21 @@
-namespace ConsoleProject.Enums;
-public enum Skills
+namespace ConsoleProject.Enums
 {
-     SocialMedia,
-     UIUx,
-     SEO,
-     ContentWriting,
-     GraphicsDesign,
+    // Skills enumeration represents different skills that can be associated with an applicant or program.
+    public enum Skills
+    {
+        // SocialMedia skill represents expertise in managing social media platforms.
+        SocialMedia,
+
+        // UIUx skill represents expertise in user interface and user experience design.
+        UIUx,
+
+        // SEO skill represents expertise in search engine optimization.
+        SEO,
+
+        // ContentWriting skill represents expertise in writing content for various purposes.
+        ContentWriting,
+
+        // GraphicsDesign skill represents expertise in graphics and visual design.
+        GraphicsDesign
+    }
 }
