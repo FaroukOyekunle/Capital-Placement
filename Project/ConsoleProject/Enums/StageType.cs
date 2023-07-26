2af2daa2c256dee5c425697fb34503eef80ec82f
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-26 11:43:25 +0100

    Add StageType enumeration
    adds the StageType enumeration to the application. The enum represents different stages in an application process. It includes stages such as Applied, VideoInterview, Offered, and Shortlisting, which are used to track the progress of applicants through various stages of the application process. These stages can be utilized in the application's applicant profiles or program details to keep track of an applicant's current stage in the application process.

--- Project/ConsoleProject/Enums/StageType.cs
+++ Project/ConsoleProject/Enums/StageType.cs
@@ -1,8 +1,18 @@
-namespace ConsoleProject.Enums;
-public enum StageType
+namespace ConsoleProject.Enums
 {
-    Applied =1,
-    VideoInterview,
-    Offered,
-    Shortlisting
+    // StageType enumeration represents different stages in an application process.
+    public enum StageType
+    {
+        // Applied stage represents the initial stage when an applicant has applied for a program.
+        Applied = 1,
+
+        // VideoInterview stage represents the stage where applicants go through a video interview.
+        VideoInterview,
+
+        // Offered stage represents the stage when an offer is extended to the applicant.
+        Offered,
+
+        // Shortlisting stage represents the stage where applicants are shortlisted based on their qualifications.
+        Shortlisting
+    }
 }
