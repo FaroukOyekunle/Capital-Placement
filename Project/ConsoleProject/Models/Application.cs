25b16b814ea438379f87888b48298f6ccb2194d6
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-27 13:02:48 +0100

    Add Application model class
    Add Application model class to the application. The Application class represents an entity that holds information about an applicant's application. It includes properties for storing the applicant's personal details such as first name, last name, email address, phone number, nationality, and more. Additionally, it contains references to related entities like Program and Stage, represented by foreign keys ProgramId and StageId. This model serves as a fundamental building block for managing application data within the application.

--- Project/ConsoleProject/Models/Application.cs
+++ Project/ConsoleProject/Models/Application.cs
@@ -1,22 +1,54 @@
 using ConsoleProject.Enums;
 using System.ComponentModel.DataAnnotations;
-namespace ConsoleProject.Models;
 
-public class Application : BaseModel
+namespace ConsoleProject.Models
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string EmailAddress { get; set; }
-    public string PhoneNumber { get; set; }
-    public string ApplicationCoverImage { get; set; }
-    public string Nationality { get; set; }
-    public string CurrentResidence { get; set; }
-    public string IdNumber { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string StageId { get; set; }
-    public ConsoleProject.Models.Program Program { get; set; }
-    public string ProgramId { get; set; }
-    public Gender Gender { get; set; }
-    public Profile Profile { get; set; }
-    public Stage Stage { get; set; }
+    // Application represents a class that holds information about an application.
+    public class Application : BaseModel
+    {
+        // FirstName represents the first name of the applicant.
+        public string FirstName { get; set; }
+
+        // LastName represents the last name of the applicant.
+        public string LastName { get; set; }
+
+        // EmailAddress represents the email address of the applicant.
+        public string EmailAddress { get; set; }
+
+        // PhoneNumber represents the phone number of the applicant.
+        public string PhoneNumber { get; set; }
+
+        // ApplicationCoverImage stores the path to the application cover image.
+        public string ApplicationCoverImage { get; set; }
+
+        // Nationality represents the nationality of the applicant.
+        public string Nationality { get; set; }
+
+        // CurrentResidence represents the current residence of the applicant.
+        public string CurrentResidence { get; set; }
+
+        // IdNumber represents the identification number of the applicant.
+        public string IdNumber { get; set; }
+
+        // DateOfBirth represents the date of birth of the applicant.
+        public DateTime DateOfBirth { get; set; }
+
+        // StageId is the foreign key referencing the associated Stage entity.
+        public string StageId { get; set; }
+
+        // Program represents the associated Program entity for the application.
+        public ConsoleProject.Models.Program Program { get; set; }
+
+        // ProgramId is the foreign key referencing the associated Program entity.
+        public string ProgramId { get; set; }
+
+        // Gender represents the gender of the applicant (e.g., Male, Female).
+        public Gender Gender { get; set; }
+
+        // Profile represents additional profile information of the applicant.
+        public Profile Profile { get; set; }
+
+        // Stage represents the associated Stage entity for the application.
+        public Stage Stage { get; set; }
+    }
 }
