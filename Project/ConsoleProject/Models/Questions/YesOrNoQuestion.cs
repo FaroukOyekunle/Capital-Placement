22d92af2d754232629a4a8858f2a20b05d1b6fa7
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-29 06:57:20 +0100

    Add YesOrNoQuestion model
    YesOrNoQuestion class represents a yes or no question in the application. It includes properties to store the applicant's choice ("Yes" or "No"), a flag to indicate disqualification for "No" choice, and navigation properties to link the question to its parent Question object.

--- Project/ConsoleProject/Models/Questions/YesOrNoQuestion.cs
+++ Project/ConsoleProject/Models/Questions/YesOrNoQuestion.cs
@@ -1,9 +1,20 @@
-namespace ConsoleProject.Models;
-
-public class YesOrNoQuestion
+namespace ConsoleProject.Models
 {
-    public bool Choice {get; set;}
-    public bool DisqualifyForNoChoice {get; set;}
-    // public Guid QuestionId {get; set;}
-    // public Question Question {get; set;}
+    // YesOrNoQuestion represents a class that holds information about a yes or no question.
+    public class YesOrNoQuestion
+    {
+        // Choice indicates whether the applicant chose "Yes" (true) or "No" (false) as the response.
+        public bool Choice { get; set; }
+
+        // DisqualifyForNoChoice is a flag that indicates whether the applicant should be disqualified
+        // if they choose "No" as the response. If true, selecting "No" leads to disqualification.
+        public bool DisqualifyForNoChoice { get; set; }
+
+        // QuestionId stores the unique identifier of the question to which this YesOrNoQuestion belongs.
+        public Guid QuestionId { get; set; }
+
+        // Question is a navigation property that references the actual Question object
+        // to which this YesOrNoQuestion is associated.
+        public Question Question { get; set; }
+    }
 }
