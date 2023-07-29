1bf783c73815e1cb8dc6880a537d30da8a5426ca
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-29 06:52:25 +0100

    Add VideoBasedQuestion model
    VideoBasedQuestion class represents a video-based question in the application. It includes properties to store additional submission information and the maximum duration of the video allowed for the response.

--- Project/ConsoleProject/Models/Questions/VideoBasedQuestion.cs
+++ Project/ConsoleProject/Models/Questions/VideoBasedQuestion.cs
@@ -1,7 +1,14 @@
-namespace ConsoleProject.Models;
-
-public class VideoBasedQuestion
+namespace ConsoleProject.Models
 {
-    public string AdditionalSubmissionInformation{get; set;}
-    public string MaxDurationOfVideo {get; set;}
+    // VideoBasedQuestion represents a class that holds information about a video-based question.
+    public class VideoBasedQuestion
+    {
+        // AdditionalSubmissionInformation stores additional information or instructions
+        // for the applicant regarding the video submission.
+        public string AdditionalSubmissionInformation { get; set; }
+
+        // MaxDurationOfVideo specifies the maximum duration allowed for the video response.
+        // It stores the duration of the video in a string format, e.g., "5 minutes".
+        public string MaxDurationOfVideo { get; set; }
+    }
 }
