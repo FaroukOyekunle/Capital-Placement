70b46762e483542347396c167b0212cdeaa681e2
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-07-29 06:27:16 +0100

    Add NumberQuestion model
    Add the NumberQuestion model to the application. The NumberQuestion class represents a numeric question in the application. It includes a property called 'QuestionNumber', which is of type int and holds the numeric value of the question.

--- Project/ConsoleProject/Models/Questions/NumberQuestion.cs
+++ Project/ConsoleProject/Models/Questions/NumberQuestion.cs
@@ -1,8 +1,16 @@
-namespace ConsoleProject.Models;
-
-public class NumberQuestion 
+namespace ConsoleProject.Models
 {
-    // public Guid QuestionId {get; set;}
-    // public Question Question {get; set;}
-    public int QuestionNumber {get; set;}
+    // NumberQuestion represents a class that holds information about a numeric question.
+    public class NumberQuestion
+    {
+        // QuestionNumber represents the numeric value of the question.
+        public int QuestionNumber { get; set; }
+
+        // QuestionId represents the unique identifier for the number question.
+        // It is of type Guid and can be used to uniquely identify a specific number question.
+        // public Guid QuestionId { get; set; }
+
+        // Question represents a reference to the parent Question object to which this number question belongs.
+        // public Question Question { get; set; }
+    }
 }
