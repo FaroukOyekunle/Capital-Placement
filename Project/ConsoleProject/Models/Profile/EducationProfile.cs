5c65f36d7b36865435874012e4cccf8d11a823f9
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-01 11:39:41 +0100

    Add EducationProfile model
    Adds the EducationProfile model to the application. The EducationProfile class represents information about an applicant's education profile and inherits common properties from the ProfileBase class.

--- Project/ConsoleProject/Models/Profile/EducationProfile.cs
+++ Project/ConsoleProject/Models/Profile/EducationProfile.cs
@@ -1,6 +1,9 @@
-namespace ConsoleProject.Models;
-
-public class EducationProfile: ProfileBase
+namespace ConsoleProject.Models
 {
-    
+    // EducationProfile represents a class that holds information about an applicant's education profile.
+    // It inherits from the ProfileBase class, which contains common properties for profile-related classes.
+    public class EducationProfile : ProfileBase
+    {
+        // The class inherits the Mandatory and Hidden properties from the ProfileBase class.
+    }
 }
