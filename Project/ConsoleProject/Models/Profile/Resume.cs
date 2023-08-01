a16f6c0fc72ef2360116c639fdac5be4e13da0f4
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-01 12:01:48 +0100

    Add Resume model
    Adds the Resume model to the application. The Resume class represents information about an applicant's resume and inherits common properties from the ProfileBase class.

--- Project/ConsoleProject/Models/Profile/Resume.cs
+++ Project/ConsoleProject/Models/Profile/Resume.cs
@@ -1,6 +1,9 @@
-namespace ConsoleProject.Models;
-
-public class Resume : ProfileBase
+namespace ConsoleProject.Models
 {
-    
+    // Resume represents a class that holds information about an applicant's resume.
+    // It inherits from the ProfileBase class, which contains common properties for profile-related classes.
+    public class Resume : ProfileBase
+    {
+        // The class inherits the Mandatory and Hidden properties from the ProfileBase class.
+    }
 }
