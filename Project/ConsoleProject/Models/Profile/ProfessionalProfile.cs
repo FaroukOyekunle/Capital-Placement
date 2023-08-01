386bfaa104fa92b07221e72ba029aab6ea0fc11e
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-01 11:58:33 +0100

    Add ProfessionalProfile model
    Adds the ProfessionalProfile model to the application. The ProfessionalProfile class represents information about an applicant's professional profile and inherits common properties from the ProfileBase class.

--- Project/ConsoleProject/Models/Profile/ProfessionalProfile.cs
+++ Project/ConsoleProject/Models/Profile/ProfessionalProfile.cs
@@ -1,6 +1,9 @@
-namespace ConsoleProject.Models;
-
-public class ProfessionalProfile: ProfileBase
+namespace ConsoleProject.Models
 {
-    
+    // ProfessionalProfile represents a class that holds information about an applicant's professional profile.
+    // It inherits from the ProfileBase class, which contains common properties for profile-related classes.
+    public class ProfessionalProfile : ProfileBase
+    {
+        // The class inherits the Mandatory and Hidden properties from the ProfileBase class.
+    }
 }
