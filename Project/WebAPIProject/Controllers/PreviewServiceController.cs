73421612c6338cd70e0fe6380f67b633b457495b
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-02 15:38:10 +0100

    Update Preview Service Controller
    Adds the Preview Service Controller class to the Web API Project Controllers class. The controller defines an HTTP route to get the program preview based on the program title using the IPreviewService interface for handling the business logic.

--- Project/WebAPIProject/Controllers/PreviewServiceController.cs
+++ Project/WebAPIProject/Controllers/PreviewServiceController.cs
@@ -2,23 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebAPIProject.Interfaces.Services;
 
-namespace WebAPIProject.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class PreviewServiceController : ControllerBase
+namespace WebAPIProject.Controllers
 {
-    private readonly IPreviewService _previewService;
+    // Mark the controller as an API controller.
+    [ApiController]
+    // Define the route for the controller.
+    [Route("api/[controller]")]
+    public class PreviewServiceController : ControllerBase
+    {
+        private readonly IPreviewService _previewService;
 
-    public PreviewServiceController(IPreviewService previewService)
-    {
-        _previewService = previewService;
-    }
-    
-    [HttpGet("GetPreview")]
-    public async Task<IActionResult> GetPreviewAsync([FromQuery] string programTitle)
-    {
-     var response = await _previewService.GetProgramPreviewAsync(programTitle);
-      return response.Status? Ok(response) : BadRequest(response.Message);
+        // Constructor injection for the service interface.
+        public PreviewServiceController(IPreviewService previewService)
+        {
+            _previewService = previewService;
+        }
+
+        // Define the HTTP route for getting the program preview by its program title.
+        [HttpGet("GetPreview")]
+        public async Task<IActionResult> GetPreviewAsync([FromQuery] string programTitle)
+        {
+            // Call the service method to get the program preview and get the response.
+            var response = await _previewService.GetProgramPreviewAsync(programTitle);
+            // Check if the operation was successful and return the appropriate result.
+            // If successful, return the response as Ok result, otherwise return a BadRequest with the error message.
+            return response.Status ? Ok(response) : BadRequest(response.Message);
+        }
     }
 }
