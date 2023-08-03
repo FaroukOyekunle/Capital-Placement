edfe2802382cc0fe107b5c7273359ffc7936e3f3
Umar al Farouk <94355638+FaroukOyekunle@users.noreply.github.com>
2023-08-03 10:54:23 +0100

    Update Application Repository
    Adds the Application Repository class that implements the IApplicationRepository interface for managing Application entities in the database using the Application Context.

--- Project/WebAPIProject/Implementations/Repositories/ApplicationRepository.cs
+++ Project/WebAPIProject/Implementations/Repositories/ApplicationRepository.cs
@@ -1,13 +1,18 @@
-﻿using ConsoleProject.Models;
+using ConsoleProject.Models;
 using WebAPIProject.Context;
-using WebAPIProject.Interfaces.Repositories;
+using WebAPIProject.Interfaces.Repositories; 
 
 namespace WebAPIProject.Implementations.Repositories
 {
+    // Define the ApplicationRepository class that implements the IApplicationRepository interface.
     public class ApplicationRepository : GenericRepository<Application>, IApplicationRepository
     {
+        // Constructor that takes an ApplicationContext instance and passes it to the base class constructor.
         public ApplicationRepository(ApplicationContext context) : base(context)
         {
+            // The "base" keyword is used to call the constructor of the base class (GenericRepository<Application>).
+            // This is necessary because the GenericRepository class takes the ApplicationContext as a parameter.
+            // The ApplicationContext is used to access the database and perform CRUD operations on the Application entity.
         }
     }
 }
